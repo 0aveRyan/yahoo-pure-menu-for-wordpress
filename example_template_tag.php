9ba28126241f95b6b13c1d298233ebbc784d10aa
David Ryan <davidryan@asu.edu>
2015-02-15 18:02:59 -0700

    Update example_template_tag.php

--- example_template_tag.php
+++ example_template_tag.php
@@ -2,12 +2,11 @@
   // use this wherever you'd like to reference your menu in your template.
   $args = array(
   	'theme_location'  => 'your-theme-location-goes-here',
-    'container'       => 'none',
+        'container'       => 'none',
 	'menu_class'      => 'pure-menu-list',
 	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
 	'depth'           => 0,
 	'walker'          => 'Simple_Pure_Walker'
   );  
   wp_nav_menu( $args ); 
-
 ?>
